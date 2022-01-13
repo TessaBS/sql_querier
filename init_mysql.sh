dff8567ad8f786ff144c8f55d6a20d256d673da1
Rijk van Putten <github@rijkvp.nl>
2022-01-13 09:06:04 +0000

    Fix mysql

--- init_mysql.sh
+++ init_mysql.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 # Run the MySQL to create the databases
 echo "Starting MySQL.."
-start_mysql.sh
+if [ ! -e /var/run/mysqld/gitpod-init.lock ]
+then
+    touch /var/run/mysqld/gitpod-init.lock
+
+    # initialize database structures on disk, if needed
+    [ ! -d /workspace/mysql ] && mysqld --initialize-insecure
+
+    # launch database, if not running
+    [ ! -e /var/run/mysqld/mysqld.pid ] && mysqld --daemonize
+
+    rm /var/run/mysqld/gitpod-init.lock
+fi
 echo "Creating MySQL databases.."
-mysql < create_dbs.sql
+mysql < ./create_dbs.sql
 echo "Done!"
