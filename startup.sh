8b6264aafef51353c966a229425c9cf7ce00cedc
Rijk van Putten <github@rijkvp.nl>
2022-01-13 18:57:03 +0000

    Add cool letters

--- startup.sh
+++ startup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-echo 'Performing startup commands..'
+echo "
+ ____   ___  _        ___                  _           
+/ ___| / _ \| |      / _ \ _   _  ___ _ __(_) ___ _ __ 
+\___ \| | | | |     | | | | | | |/ _ \ '__| |/ _ \ '__|
+ ___) | |_| | |___  | |_| | |_| |  __/ |  | |  __/ |   
+|____/ \__\_\_____|  \__\_\\__,_|\___|_|  |_|\___|_|   
+
+Door: Rijk van Putten
+"
+                                       
 ./init_mysql.sh
 sleep 1
 echo 'Install node packages..'
