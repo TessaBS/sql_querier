b0cd8f972f2f3d57b9e5a990631bb6dfeb4e9dc7
Rijk van Putten <github@rijkvp.nl>
2022-01-13 08:44:09 +0000

    Fix gitpod config 3

--- init_mysql.sh
+++ init_mysql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Run the MySQL to create the databases
-echo "Starting MySQL server.."
-start_mysql.sh
+echo "MySQL should be running"
+mysql
 echo "Creating MySQL databases.."
 mysql < create_dbs.sql
 echo "Done!"
